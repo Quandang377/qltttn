93d00abf2a8a685e74a6b2b9ed98896ede88aa82
ThanksKiet <0306211159.caothang.edu.vn>
2025-06-16 18:53:21 +0700

    merge kiet to main

--- includes/ThongBao_funtions.php
+++ includes/ThongBao_funtions.php
@@ -1,9 +1,5 @@
 <?php 
-<<<<<<< HEAD
 $stmt = $conn->prepare("SELECT ID, TIEUDE, NOIDUNG, NGAYDANG FROM THONGBAO WHERE TRANGTHAI=1 ORDER BY NGAYDANG DESC");
-=======
-$stmt = $conn->prepare("SELECT ID, TIEUDE, NOIDUNG, NGAYDANG FROM THONGBAO ORDER BY NGAYDANG DESC");
->>>>>>> 4fd8ce05db2488642b901eba16148a94e291076e
 $stmt->execute();
 $thongbaos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
