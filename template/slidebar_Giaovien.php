6643be80e0a5ff8e7bb544298d84142cafa8117e
ThanksKiet <0306211159.caothang.edu.vn>
2025-07-06 12:25:16 +0700

    chụpgiaodien

--- template/slidebar_Giaovien.php
+++ template/slidebar_Giaovien.php
@@ -2,35 +2,33 @@
     <div class="navbar-header">
         <a class="navbar-brand">Cao đẳng kỹ thuật Cao Thắng</a>
     </div>
-    <aside class="sidebar navbar-default" role="navigation">
-        <div class="sidebar-nav navbar-collapse">
-            <ul class="nav" id="side-menu">
-                </li>
-                <li>
-                    <a href="pages/giaovien/trangchu" class="active"><i class="fa fa-dashboard fa-fw"></i> Trang chủ</a>
-                </li>
-                <li>
-                    <a href="pages/giaovien/quanlygiaygioithieu" class="active"><i class="fa fa-dashboard fa-fw"></i>
-                        Quản lý giấy giới thiệu</a>
-                </li>
-                <li>
-                    <a href="pages/giaovien/xemdanhsachsinhvien" class="active"><i class="fa fa-dashboard fa-fw"></i>
-                        Danh sách sinh viên</a>
-                </li>
-                <li>
-                    <a href="pages/giaovien/khaosat" class="active"><i class="fa fa-dashboard fa-fw"></i> Khảo sát</a>
-                </li>
-                <li>
-                    <a href="pages/giaovien/xemdanhsachcongty" class="active"><i class="fa fa-dashboard fa-fw"></i> Danh
-                        sách công ty</a>
-                </li>
-                <li>
-                    <a href="pages/giaovien/thongtincanhan">
-                        <i class="fa fa-user fa-fw"></i> Thông tin cá nhân
-                    </a>
-                </li>
-                <li><a href="/datn/logout"><i class="fa fa-sign-out"></i> Đăng xuất</a></li>
-            </ul>
-        </div>
-    </aside>
+    <aside class="navbar-default sidebar" role="navigation">
+    <div class="sidebar-nav navbar-collapse collapse" id="sidebar-collapse">
+        <ul class="nav" id="side-menu">
+            <li>
+                <a href="pages/giaovien/trangchu" ><i class="fa fa-home fa-fw"></i> Trang chủ</a>
+            </li>
+            <li>
+                <a href="pages/giaovien/xembaocaosinhvien" ><i class="fa fa-file fa-fw"></i>
+                    Xem báo cáo tuần</a>
+            </li>
+            <li>
+                <a href="pages/giaovien/xemdanhsachsinhvien" ><i class="fa fa-user fa-fw"></i>
+                    Danh sách sinh viên</a>
+            </li>
+            <li>
+                <a href="pages/giaovien/khaosat" ><i class="fa fa-table fa-fw"></i> Khảo sát</a>
+            </li>
+            <li>
+                <a href="pages/giaovien/xembaocaotongket" ><i class="fa fa-dashboard fa-fw"></i> Xem báo cáo tổng kết</a>
+            </li>
+            <li>
+                <a href="pages/giaovien/thongtincanhan">
+                    <i class="fa fa-user fa-fw"></i> Thông tin cá nhân
+                </a>
+            </li>
+            <li><a href="/datn/logout"><i class="fa fa-sign-out"></i> Đăng xuất</a></li>
+        </ul>
+    </div>
+</aside>
 </nav>
